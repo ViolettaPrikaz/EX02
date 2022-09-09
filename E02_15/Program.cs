8f11658ec4d523e2607656d71cb4f605d4fce22f
Violetta <viopria@gmail.com>
2022-09-09 19:26:21 +0300

    E02_13

---
+++ E02_15/Program.cs
@@ -0,0 +1,13 @@
+﻿int number1 = new Random().Next(1, 7);
+Console.WriteLine(number1);
+
+if(number1 >= 6)
+{
+    Console.WriteLine("выходной");
+}
+else
+{
+     Console.WriteLine("рабочий");
+}
+Console.ReadKey();
+
