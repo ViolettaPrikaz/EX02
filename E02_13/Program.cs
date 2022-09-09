783b792d5614b66eb9a4d036a7d669a0c5411f01
Violetta <viopria@gmail.com>
2022-09-09 19:25:49 +0300

    E02_13

---
+++ E02_13/Program.cs
@@ -0,0 +1,13 @@
+﻿int number1 = new Random().Next(50, 200);
+Console.WriteLine(number1);
+
+if(number1 > 99)
+{
+    Console.WriteLine(number1.ToString()[2]);
+}
+else
+{
+     Console.WriteLine("третьей цифры нет");
+}
+Console.ReadKey();
+
